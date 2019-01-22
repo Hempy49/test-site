0425445e70cedec3d612422c8eac2095a7fa5e55
Millie Clare | Decibel <mclare@decibelinsight.com>
2019-01-22 16:29:48 +0000

    SDTS-8 phone photos updated

--- src/api/mockPhones.js
+++ src/api/mockPhones.js
@@ -1,38 +1,129 @@
+
 export default [
     {
         id: '1',
         name: 'iPhone 6',
         price: 500,
-        image: 'uploads/iphone.png'
+        image: 'uploads/apple_iphone_6.png'
     },
     {
         id: '2',
-        name: 'iPhone 8',
+        name: 'iPhone 7',
         price: 800,
-        image:'uploads/iphone.png'
+        image:'uploads/apple_iphone_7.png'
     },
     {
         id: '3',
-        name: 'iPhone X',
+        name: 'iPhone 8',
         price: 1000,
-        image:'uploads/iphone.png'
+        image:'uploads/apple_iphone_8.png'
     },
     {
         id: '4',
-        name: 'iPhone 5s',
+        name: 'iPhone X',
         price: 400,
-        image: 'uploads/iphone.png'
+        image: 'uploads/apple_iphone_X.png'
     },
     {
         id: '5',
-        name: 'iPhone 5',
+        name: 'iPhone XR',
         price: 300,
-        image:'uploads/iphone.png'
+        image:'uploads/apple_iphone_XR.png'
     },
     {
         id: '6',
-        name: 'iPhone 4',
+        name: 'iPhone XS',
         price: 200,
-        image:'uploads/iphone.png'
+        image:'uploads/apple_iphone_XS.png'
+    }, 
+    {
+        id: '7',
+        name: 'Samsung Galaxy S8',
+        price: 400,
+        image: 'uploads/samsung_galaxy_S8.png'
+    },
+    {
+        id: '8',
+        name: 'iSamsung Galaxy A7',
+        price: 300,
+        image:'uploads/samsung_galaxy_A7.png'
+    },
+    {
+        id: '9',
+        name: 'Samsung Galaxy S9',
+        price: 200,
+        image:'uploads/samsung_galaxy_S9.png'
+    }, 
+    {
+        id: '10',
+        name: 'Samsung Galaxy A9',
+        price: 400,
+        image: 'uploads/samsung_galaxy_A9.png'
+    },
+    {
+        id: '11',
+        name: 'Samsung Galaxy J4+',
+        price: 300,
+        image:'uploads/samsung_galaxy_j4+.png'
+    },
+    {
+        id: '12',
+        name: 'Samsung Galaxy Note9',
+        price: 200,
+        image:'uploads/samsung_galaxy_note9.png'
+    }, 
+    {
+        id: '13',
+        name: 'Huawei P20 lite',
+        price: 400,
+        image: 'uploads/huawei_P20-lite.png'
+    },
+    {
+        id: '14',
+        name: 'Huawei P20 Pro',
+        price: 300,
+        image:'uploads/huawei_p20_pro.png'
+    },
+    {
+        id: '15',
+        name: 'Huawei P smart 2019',
+        price: 200,
+        image:'uploads/huawei_p_smart_2019.png'
+    },
+    {
+        id: '16',
+        name: 'Huawei Mate 20 Pro',
+        price: 300,
+        image:'uploads/huawei_mate_20_pro.png'
+    }, 
+    {
+        id: '17',
+        name: 'Google Pixel 3',
+        price: 200,
+        image:'uploads/google_pixel_3.png'
+    }, 
+    {
+        id: '18',
+        name: 'Google Pixel 3 XL',
+        price: 400,
+        image: 'uploads/google_pixel_3_XL.png'
+    }, 
+    {
+        id: '19',
+        name: 'BlackBerry KEY2',
+        price: 300,
+        image:'uploads/blackberry_key_2.png'
+    },
+    {
+        id: '20',
+        name: 'Nokia 7.1',
+        price: 200,
+        image:'uploads/nokia_7.1.png'
+    },
+    {
+        id: '21',
+        name: 'Sony Xperia XZ3',
+        price: 200,
+        image:'uploads/sony_xperia_XZ3.png'
     }
 ]
